635e8fb2485636dc6a825d8b79363640bb7de5ea
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2023-10-14 15:00:02 -0400

    feat: chat scrolling

--- src/components/container/ChatContainer/index.tsx
+++ src/components/container/ChatContainer/index.tsx
@@ -1,19 +1,84 @@
 import ChatBubble from "@/components/ChatBubble"
 import { Chat } from "@/lib/types"
+import { useEffect, useRef, useState } from "react"
 
 interface ChatContainerProps {
-    chats: Chat[]
+    
 }
 
-export default function ChatContainer({ chats }: ChatContainerProps) {
+export default function ChatContainer({ }: ChatContainerProps) {
+    const [chats, setChats] = useState<Chat[]>([
+        {
+          sender: "You",
+          text: "Should I buy this stock?",
+          left: false,
+        },
+        {
+          sender: "ChatGPT",
+          text: "Buying AAPL stocks is generally safe.",
+          left: true,
+        },
+        {
+          sender: "You",
+          text: "Is it likely the price will go up soon?",
+          left: false,
+        },
+        {
+          sender: "ChatGPT",
+          text: "Based on current market sentiment, it's likely that AAPL will go up in price.",
+          left: true
+        },
+        {
+          sender: "You",
+          text: "Should I buy this stock?",
+          left: false,
+        },
+        {
+          sender: "ChatGPT",
+          text: "Buying AAPL stocks is generally safe.",
+          left: true,
+        },
+        {
+          sender: "You",
+          text: "Is it likely the price will go up soon?",
+          left: false,
+        },
+        {
+          sender: "ChatGPT",
+          text: "Based on current market sentiment, it's likely that AAPL will go up in price.",
+          left: true
+        }
+      ])
+    const sendChat = (chat: Chat): void => {
+      let newChats = chats.map((c) => c)
+      newChats.push(chat)
+      setChats(newChats)
+    }
+    const chatboxRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        chatboxRef.current && chatboxRef.current!.scrollIntoView({ behavior: "smooth" })
+    }, [chats])
     let chatComponents: React.JSX.Element[] = []
     chats.forEach((chat) => chatComponents.push(<ChatBubble key={Math.random()} chat={chat} color={chat.sender === "You" ? "bg-blue-500" : "bg-gray-500"} />))
     return (
         <div className="flex flex-col m-0 p-0 w-96 h-screen bg-gray-500">
             <div className="items-center m-0 p-0 w-full h-16 bg-red-500"/>
-            <div className="scrollbar flex-grow m-0 p-0 w-full h-auto bg-green-500 overflow-y-auto">{chatComponents}</div>
+            <div className="scrollbar flex-grow m-0 p-0 w-full h-auto bg-green-500 overflow-y-auto">
+                {chatComponents}
+                <div ref={chatboxRef} />
+            </div>
             <div className="flex m-0 p-0 items-center justify-center w-full h-fit bg-blue-500">
-                <input className="rounded-lg block text-black w-full h-fit p-2 bg-gray-200 outline-none" type="text" placeholder="Send a message" />
+                <input className="rounded-lg block text-black w-full h-fit p-2 bg-gray-200 outline-none" type="text" placeholder="Send a message" onKeyDown={(e) => {
+                    const value = e.currentTarget.value
+                    if (e.key !== "Enter" || !value) return
+                    e.currentTarget.value = ""
+                    const chat = {
+                        sender: "You",
+                        text: value,
+                        left: false
+                    }
+                    sendChat(chat)
+                }} />
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,23 @@
 import ChatContainer from "@/components/container/ChatContainer";
 import MainContainer from "@/components/container/MainContainer";
+import { Chat } from "@/lib/types";
+import { useState } from "react";
 
-export default function Home() {
-  const chats = [
-    {
-      sender: "You",
-      text: "Should I buy this stock?",
-      left: false,
-    },
-    {
-      sender: "ChatGPT",
-      text: "Buying AAPL stocks is generally safe.",
-      left: true,
-    },
-    {
-      sender: "You",
-      text: "Is it likely the price will go up soon?",
-      left: false,
-    },
-    {
-      sender: "ChatGPT",
-      text: "Based on current market sentiment, it's likely that AAPL will go up in price.",
-      left: true
-    },
-    {
-      sender: "You",
-      text: "Should I buy this stock?",
-      left: false,
-    },
-    {
-      sender: "ChatGPT",
-      text: "Buying AAPL stocks is generally safe.",
-      left: true,
-    },
-    {
-      sender: "You",
-      text: "Is it likely the price will go up soon?",
-      left: false,
-    },
-    {
-      sender: "ChatGPT",
-      text: "Based on current market sentiment, it's likely that AAPL will go up in price.",
-      left: true
-    }
-  ]
+interface HomeProps {
+  
+}
+
+export default function Home({ }: HomeProps) {
   return (
     <div className="flex flex-row m-0 p-0 w-screen h-screen text-custom-gray-4">
-      <ChatContainer chats={chats} />
+      <ChatContainer />
       <MainContainer />
     </div>
   )
+}
+
+export function getServersideProps(): HomeProps {
+  return {
+    
+  }
 }
