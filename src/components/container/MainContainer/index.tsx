d402065c4db51dfd734e3291a004d105c97e9e5a
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2023-10-14 18:30:56 -0400

    save

--- src/components/container/MainContainer/index.tsx
+++ src/components/container/MainContainer/index.tsx
@@ -1,20 +1,19 @@
 import Info from "@/components/Info";
 import Toolbar from "@/components/Toolbar";
 import Watchlist from "@/components/Watchlist";
+import { Stock } from "@/lib/types";
 
 interface MainContainerProps {
-
+    stocks: Stock[]
+    currentStockIndex: number
 }
 
-export default function MainContainer({}: MainContainerProps) {
-    let prices = []
-    for (let i = 0; i < 150; i++) {
-        prices.push(Math.random() * 10)
-    }
+export default function MainContainer({ stocks, currentStockIndex }: MainContainerProps) {
+    let prices = stocks[currentStockIndex].prices ?? []
     return (
         <div className="scrollbar flex flex-grow flex-col m-0 p-0 w-auto h-full bg-gray-100 overflow-y-auto overflow-x-hidden items-start">
             <Toolbar />
-            <Watchlist />
+            <Watchlist watched={stocks} />
             <Info stock={{
                 ticker: "AAPL",
                 name: "Apple",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,57 @@
 import ChatContainer from "@/components/container/ChatContainer";
 import MainContainer from "@/components/container/MainContainer";
-import { Chat } from "@/lib/types";
+import { Chat, Stock } from "@/lib/types";
 import { useState } from "react";
 
+
 interface HomeProps {
-  
+  stocks: Stock[]
 }
 
-export default function Home({ }: HomeProps) {
+export default function Home({ stocks }: HomeProps) {
+  return <div />
   return (
     <div className="flex flex-row m-0 p-0 w-screen h-screen text-custom-gray-4">
       <ChatContainer />
-      <MainContainer />
+      <MainContainer stocks={[]} currentStockIndex={0} />
     </div>
   )
 }
 
-export function getServersideProps(): HomeProps {
+export async function getStaticProps() {
+  const watchedTickers = ["TSLA", "NFLX", "AMD", "MSFT", "AAPL"]
+  console.log(watchedTickers)
+  const stocks: Stock[] = []
+  fetch("/api/today?stock=AAPL", {
+    mode: "no-cors"
+  }).then((res) => console.log(res))
+
+  /*await watchedTickers.forEach(async (ticker) => {
+    let res = await fetch("localhost:3000/api/today", {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify({
+        stock: ticker
+      })
+    })
+    const data = await res.json()
+    if (res.status !== 200) {
+      console.error(res.status, res.statusText, data.error)
+    }
+    const prices = data.prices
+    stocks.push({
+      ticker,
+      name: "AAPL",
+      delta: prices.delta,
+      price: prices.price,
+      prices
+    })
+  })*/
+  console.log(stocks)
+
   return {
-    
+    props: {
+      stocks
+    }
   }
 }
