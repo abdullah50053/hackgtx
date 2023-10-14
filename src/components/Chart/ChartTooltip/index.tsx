4028af28bed15da868d2fb7d7fd85a647c5073f2
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2023-10-14 17:44:39 -0400

    feat: text color for tooltip

--- src/components/Chart/ChartTooltip/index.tsx
+++ src/components/Chart/ChartTooltip/index.tsx
@@ -7,7 +7,7 @@ interface ChartTooltipProps {
 export default function ChartTooltip({ active, payload, label }: ChartTooltipProps) {
     if (!active || !payload || !payload.length || !label) return <></>
     return (
-        <div className="bg-gray-500 bg-opacity-80 p-1 rounded-lg">
+        <div className="bg-gray-500 text-white bg-opacity-80 p-1 rounded-lg">
             <p>{label.toLocaleTimeString()}</p>
             <p>${payload[0].value.toFixed(2)}</p>
         </div>
