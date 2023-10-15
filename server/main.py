d6f34dcb3d0bfaf6a6cb8c9f3e63d986c0fc4782
Michael Zhao <michaelzhao314@gmail.com>
2023-10-15 06:43:58 -0400

    Added 2 other prompts and default prompt

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from flask import Flask
+from flask import Flask, request
 from models.predict import predict
 from util import ai
 
@@ -19,3 +19,20 @@ def predict_stock(stock: str):
 def chat(ticker: str, experience: str):
     return ai.run_do_i_buy(ticker=ticker.upper(), experience=experience)
 
+
+@app.post("/chat/buywhen/<ticker>/<experience>")
+def chat1(ticker: str, experience: str):
+    return ai.run_when_to_buy(ticker=ticker.upper(), experience=experience)
+
+
+@app.post("/chat/info/<ticker>/<experience>")
+def chat2(ticker: str, experience: str):
+    return ai.run_get_info(ticker=ticker.upper(), experience=experience)
+
+@app.post("/chat/any")
+def chat3():
+    # Get body
+    body = request.get_json()
+    data = body["data"]
+
+    return ai.run_any(data)
