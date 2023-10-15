103dd252feb8d44b3caa1163e53123570d08cd97
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2023-10-15 01:43:16 -0400

    feat: search

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -14,13 +14,13 @@ const Chart = dynamic(
 
 interface InfoProps {
     user?: UserData
-    setUser: any
+    updateWatchlist: any
     stock: Stock
     prices: number[]
     lastUpdate: Date
 }
 
-export default function Info({ user, setUser, stock, prices, lastUpdate }: InfoProps) {
+export default function Info({ user, updateWatchlist, stock, prices, lastUpdate }: InfoProps) {
     if (!stock) return <div className="w-full"/>
     return (
         <div className="rounded-3xl flex flex-col w-11/12 my-6 mx-auto p-4 text-black bg-white">
@@ -54,13 +54,13 @@ export default function Info({ user, setUser, stock, prices, lastUpdate }: InfoP
         {user && <div className="flex flex-row items-center mx-auto mt-4 w-full">
           {user.watchlist.includes(stock.ticker) ? <div className="flex flex-row items-center justify-center cursor-pointer w-max p-1 px-2 rounded-xl border-4 border-red-400 text-red-400 hover:border-transparent hover:text-white hover:bg-red-400 transition" onClick={() => {
             user.watchlist = user.watchlist.filter((f) => f !== stock.ticker);
-            setUser(user)
+            updateWatchlist(user)
           }}>
               <MinusSVG className="w-6 h-6 mr-2" />
               <div className="text-sm font-extrabold">Unwatch</div>
           </div> : <div className="flex flex-row items-center justify-center cursor-pointer w-max p-1 px-2 rounded-xl border-4 border-green-400 text-green-400 hover:border-transparent hover:text-white hover:bg-green-400 transition" onClick={() => {
               user.watchlist = user.watchlist.concat(stock.ticker);
-              setUser(user)
+              updateWatchlist(user)
           }}>
               <PlusSVG className="w-6 h-6 mr-2" />
               <div className="text-sm font-extrabold">Watch</div>
--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -5,19 +5,31 @@ import AngleSVG from "../svg/AngleSVG";
 import ProfileSVG from "../svg/ProfileSVG";
 import BellSVG from "../svg/BellSVG";
 import { UserData } from "@/lib/user";
+import { Stock } from "@/lib/types";
 
 interface ToolbarProps {
     user?: UserData
+    stocks: Stock[]
+    setCurrentStockIndex: any
 }
 
-export default function Toolbar({ user }: ToolbarProps) {
+export default function Toolbar({ user, stocks, setCurrentStockIndex }: ToolbarProps) {
     const [selected, setSelected] = useState<boolean>(false)
     return (
         <div className="flex flex-shrink-0 flex-row content-start items-center w-full h-20 px-8 bg-white">
             {/* Search Bar */}
             <div className={`rounded-lg transition flex flex-row m-auto justify-center w-1/2 h-1/2 overflow-hidden bg-gray-200 ${selected ? "brightness-95" : "brightness-100"}`}>
                 <SearchSVG className={`my-auto mx-2 text-lg ${selected ? "scale-125" : "scale-100"}`} />
-                <input className="flex-grow outline-none pl-2 text-black bg-gray-200" type="text" placeholder="Search stock..." onFocus={() => setSelected(true)} onBlur={() => setSelected(false)} />
+                <input className="flex-grow outline-none pl-2 text-black bg-gray-200" type="text" placeholder="Search stock..." onFocus={() => setSelected(true)} onBlur={() => setSelected(false)} onKeyDown={(e) => {
+                    if (e.key !== "Enter" || stocks.length === 0) return
+                    let stockIndex = -1
+                    stocks.forEach((s, i) => {
+                        if (s.ticker === e.currentTarget.value.toUpperCase()) stockIndex = i
+                    })
+                    if (stockIndex === -1) return
+                    e.currentTarget.value = ""
+                    setCurrentStockIndex(stockIndex)
+                }} />
             </div>
             {/* Personalization */}
             <div className="flex flex-row flex-grow m-auto justify-end items-center">
