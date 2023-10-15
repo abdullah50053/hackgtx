62294b1e69af87faabceb161dbe40ff37fc808f6
Michael Zhao <michaelzhao314@gmail.com>
2023-10-15 03:39:12 -0400

    Added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Script src="https://googletagmanager.com/gtag/js?id=G-YZFWTE1XFD" strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-YZFWTE1XFD');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
